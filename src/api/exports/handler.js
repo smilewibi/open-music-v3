31072c28363fd1539618648850bce35953b7fb45
ismail <wibidjokam@gmail.com>
2021-08-15 12:25:18 +0700

    update open handler open music

--- src/api/exports/handler.js
+++ src/api/exports/handler.js
@@ -10,45 +10,25 @@ class ExportsHandler {
   }
 
   async postExportSongsHandler(request, h) {
-    try {
-      this._validator.validateExportSongsPayload(request.payload);
-      const { playlistId } = request.params;
-      const { id: userId } = request.auth.credentials;
+    this._validator.validateExportSongsPayload(request.payload);
+    const { playlistId } = request.params;
+    const { id: userId } = request.auth.credentials;
 
-      await this._playlistsService.verifyPlaylistAccess(playlistId, userId);
+    await this._playlistsService.verifyPlaylistAccess(playlistId, userId);
 
-      const message = {
-        playlistId,
-        targetEmail: request.payload.targetEmail,
-      };
+    const message = {
+      playlistId,
+      targetEmail: request.payload.targetEmail,
+    };
 
-      await this._service.sendMessage('export:songs', JSON.stringify(message));
+    await this._service.sendMessage('export:songs', JSON.stringify(message));
 
-      const response = h.response({
-        status: 'success',
-        message: 'Permintaan Anda dalam antrean',
-      });
-      response.code(201);
-      return response;
-    } catch (error) {
-      if (error instanceof ClientError) {
-        const response = h.response({
-          status: 'fail',
-          message: error.message,
-        });
-        response.code(error.statusCode);
-        return response;
-      }
-
-      // Server ERROR!
-      const response = h.response({
-        status: 'error',
-        message: 'Maaf, terjadi kegagalan pada server kami.',
-      });
-      response.code(500);
-      console.error(error);
-      return response;
-    }
+    const response = h.response({
+      status: 'success',
+      message: 'Permintaan Anda dalam antrean',
+    });
+    response.code(201);
+    return response;
   }
 }
 
